4960778af86ded63b5c1ff22fb031e94d40580c9
mitegab <104057957+mitegab@users.noreply.github.com>
2024-04-18 09:16:00 +0300

    merging the template(base.html) with the view

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    name = request.GET.get("name")  or "World"
-    return HttpResponse("Hello, {}!".format(name))
+    return render(request, 'base.html')
