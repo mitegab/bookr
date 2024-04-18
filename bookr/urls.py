392ce8c32f6650fa12cd4cffedd1b97664a14349
mitegab <104057957+mitegab@users.noreply.github.com>
2024-04-18 10:35:38 +0300

    using the queryset dict for search results

--- bookr/urls.py
+++ bookr/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path
 import reviews.views
@@ -6,4 +5,5 @@ import reviews.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', reviews.views.index),
+    path('book-search/', reviews.views.book_search)
 ]
